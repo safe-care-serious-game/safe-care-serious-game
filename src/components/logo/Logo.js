51a1f6976e2cba23d5f6cffb48c06fd9cac9041c
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2021-07-20 00:24:43 -0300

    Add game logo component

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,12 @@
+import './Logo.css';
+
+function Logo() {
+  return (
+    <div className="Logo">
+        <h1>Safe Care</h1>
+        <h2>SERIOUS GAME</h2>
+    </div>
+  );
+}
+
+export default Logo;

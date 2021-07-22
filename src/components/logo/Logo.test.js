cb4cc961f9ca8ab08ef488216eeafdede181bd8e
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2021-07-22 00:00:02 -0300

    Improve testing

--- src/components/logo/Logo.test.js
+++ src/components/logo/Logo.test.js
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Logo from './Logo';
 
 test('renders game title and subtitle', () => {
-  render(<Logo />);
-  let headerElement = screen.getByText(/safe care/i);
-  expect(headerElement).toBeInTheDocument();
-  headerElement = screen.getByText(/serious game/i);
-  expect(headerElement).toBeInTheDocument();
+  const {container} = render(<Logo />);
+  expect(container.firstChild).toHaveClass('Logo')
+  expect(container.firstChild.firstChild.tagName.toLowerCase()).toEqual('h1')
+  expect(container.firstChild.firstChild).toHaveTextContent('Safe Care')
+  expect(container.firstChild.lastChild.tagName.toLowerCase()).toEqual('h2')
+  expect(container.firstChild.lastChild).toHaveTextContent('SERIOUS GAME')
 });
--- src/components/splash-screen/SplashScreen.test.js
+++ src/components/splash-screen/SplashScreen.test.js
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react';
 import SplashScreen from './SplashScreen';
 
 test('renders splash screen', () => {
-  render(<SplashScreen />);
-  let headerElement = screen.getByText(/safe care/i);
-  expect(headerElement).toBeInTheDocument();
-  headerElement = screen.getByText(/serious game/i);
-  expect(headerElement).toBeInTheDocument();
+  const {container} = render(<SplashScreen />);
+  expect(container.firstChild).toHaveClass('Logo-Wrapper')
+  expect(container.firstChild.firstChild).toHaveClass('Logo')
+  expect(container.firstChild.firstChild.firstChild.tagName.toLowerCase()).toEqual('h1')
+  expect(container.firstChild.firstChild.firstChild).toHaveTextContent('Safe Care')
+  expect(container.firstChild.firstChild.lastChild.tagName.toLowerCase()).toEqual('h2')
+  expect(container.firstChild.firstChild.lastChild).toHaveTextContent('SERIOUS GAME')
 });

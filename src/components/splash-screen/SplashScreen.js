83dadc448a43057869c613a50bbc47873268ae97
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2021-07-24 16:03:00 -0300

    Add press to start button

--- src/components/splash-screen/SplashScreen.js
+++ src/components/splash-screen/SplashScreen.js
@@ -1,10 +1,16 @@
+import { useHistory } from 'react-router-dom';
 import Logo from '../logo/Logo';
 import './SplashScreen.css';
 
 function SplashScreen() {
+  const history = useHistory();
+
   return (
-    <div className="Logo-Wrapper">
-      <Logo/>
+    <div className="Splash-Screen" onClick={() => history.replace('/menu')}>
+      <div className="Splash-Screen-Logo-Wrapper">
+        <Logo/>
+      </div>
+      <h1 className="Splash-Screen-Press-To-Start">Clique para começar</h1>
     </div>
   );
 }
--- src/components/splash-screen/SplashScreen.test.js
+++ src/components/splash-screen/SplashScreen.test.js
@@ -1,12 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import SplashScreen from './SplashScreen';
 
 test('renders splash screen', () => {
   const {container} = render(<SplashScreen />);
-  expect(container.firstChild).toHaveClass('Logo-Wrapper')
-  expect(container.firstChild.firstChild).toHaveClass('Logo')
-  expect(container.firstChild.firstChild.firstChild.tagName.toLowerCase()).toEqual('h1')
-  expect(container.firstChild.firstChild.firstChild).toHaveTextContent('Safe Care')
-  expect(container.firstChild.firstChild.lastChild.tagName.toLowerCase()).toEqual('h2')
-  expect(container.firstChild.firstChild.lastChild).toHaveTextContent('SERIOUS GAME')
+  expect(container.firstChild).toHaveClass('Splash-Screen');
+  expect(container.firstChild.firstChild).toHaveClass('Splash-Screen-Logo-Wrapper');
+  expect(container.firstChild.firstChild.firstChild).toHaveClass('Logo');
+  expect(container.firstChild.firstChild.firstChild.firstChild.tagName.toLowerCase()).toEqual('h1');
+  expect(container.firstChild.firstChild.firstChild.firstChild).toHaveTextContent('Safe Care');
+  expect(container.firstChild.firstChild.firstChild.lastChild.tagName.toLowerCase()).toEqual('h2');
+  expect(container.firstChild.firstChild.firstChild.lastChild).toHaveTextContent('SERIOUS GAME');
+  expect(container.firstChild.lastChild).toHaveClass('Splash-Screen-Press-To-Start');
 });

5c6a6dbd9fed6aad389692bed0da7add7ca5ecd6
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2021-07-24 16:03:36 -0300

    Add support to onClick from props

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,12 +2,18 @@ import './Card.css';
 
 function Card(props) {
 
+    let onClick = () => {
+        if (props.onClick) {
+            props.onClick()
+        }
+    }
+
     let cardImageStyle = {
         backgroundImage: `url(${props.image ? props.image : ''})`
     }
 
     return (
-        <div className="Card">
+        <div className="Card" onClick={onClick}>
             <div className="Card-Image" style={cardImageStyle}></div>
             <div className="Card-Title">{props.title}</div>
         </div>

b2c4293d027fcc9fdf8863be162ab7c48fe177b9
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2023-01-08 15:52:38 -0300

    Prefer WebM over Ogg

--- src/data/level4.js
+++ src/data/level4.js
@@ -1,51 +1,51 @@
 import Level4Shot1Mp4 from '../videos/level4/Level4Shot1.mp4'
-import Level4Shot1Ogv from '../videos/level4/Level4Shot1.ogv'
 import Level4Shot1WebM from '../videos/level4/Level4Shot1.webm'
+import Level4Shot1Ogv from '../videos/level4/Level4Shot1.ogv'
 import Level4Shot2Mp4 from '../videos/level4/Level4Shot2.mp4'
-import Level4Shot2Ogv from '../videos/level4/Level4Shot2.ogv'
 import Level4Shot2WebM from '../videos/level4/Level4Shot2.webm'
+import Level4Shot2Ogv from '../videos/level4/Level4Shot2.ogv'
 import Level4Shot3Mp4 from '../videos/level4/Level4Shot3.mp4'
-import Level4Shot3Ogv from '../videos/level4/Level4Shot3.ogv'
 import Level4Shot3WebM from '../videos/level4/Level4Shot3.webm'
+import Level4Shot3Ogv from '../videos/level4/Level4Shot3.ogv'
 import Level4Shot4Mp4 from '../videos/level4/Level4Shot4.mp4'
-import Level4Shot4Ogv from '../videos/level4/Level4Shot4.ogv'
 import Level4Shot4WebM from '../videos/level4/Level4Shot4.webm'
+import Level4Shot4Ogv from '../videos/level4/Level4Shot4.ogv'
 import Level4Shot5Mp4 from '../videos/level4/Level4Shot5.mp4'
-import Level4Shot5Ogv from '../videos/level4/Level4Shot5.ogv'
 import Level4Shot5WebM from '../videos/level4/Level4Shot5.webm'
+import Level4Shot5Ogv from '../videos/level4/Level4Shot5.ogv'
 import Level4Shot6Mp4 from '../videos/level4/Level4Shot6.mp4'
-import Level4Shot6Ogv from '../videos/level4/Level4Shot6.ogv'
 import Level4Shot6WebM from '../videos/level4/Level4Shot6.webm'
+import Level4Shot6Ogv from '../videos/level4/Level4Shot6.ogv'
 import Level4Shot7Mp4 from '../videos/level4/Level4Shot7.mp4'
-import Level4Shot7Ogv from '../videos/level4/Level4Shot7.ogv'
 import Level4Shot7WebM from '../videos/level4/Level4Shot7.webm'
+import Level4Shot7Ogv from '../videos/level4/Level4Shot7.ogv'
 import Level4Shot8Mp4 from '../videos/level4/Level4Shot8.mp4'
-import Level4Shot8Ogv from '../videos/level4/Level4Shot8.ogv'
 import Level4Shot8WebM from '../videos/level4/Level4Shot8.webm'
+import Level4Shot8Ogv from '../videos/level4/Level4Shot8.ogv'
 import Level4Shot9Mp4 from '../videos/level4/Level4Shot9.mp4'
-import Level4Shot9Ogv from '../videos/level4/Level4Shot9.ogv'
 import Level4Shot9WebM from '../videos/level4/Level4Shot9.webm'
+import Level4Shot9Ogv from '../videos/level4/Level4Shot9.ogv'
 import Level4Shot10Mp4 from '../videos/level4/Level4Shot10.mp4'
-import Level4Shot10Ogv from '../videos/level4/Level4Shot10.ogv'
 import Level4Shot10WebM from '../videos/level4/Level4Shot10.webm'
+import Level4Shot10Ogv from '../videos/level4/Level4Shot10.ogv'
 import Level4Shot11Mp4 from '../videos/level4/Level4Shot11.mp4'
-import Level4Shot11Ogv from '../videos/level4/Level4Shot11.ogv'
 import Level4Shot11WebM from '../videos/level4/Level4Shot11.webm'
+import Level4Shot11Ogv from '../videos/level4/Level4Shot11.ogv'
 import Level4Shot12Mp4 from '../videos/level4/Level4Shot12.mp4'
-import Level4Shot12Ogv from '../videos/level4/Level4Shot12.ogv'
 import Level4Shot12WebM from '../videos/level4/Level4Shot12.webm'
+import Level4Shot12Ogv from '../videos/level4/Level4Shot12.ogv'
 import Level4Shot13Mp4 from '../videos/level4/Level4Shot13.mp4'
-import Level4Shot13Ogv from '../videos/level4/Level4Shot13.ogv'
 import Level4Shot13WebM from '../videos/level4/Level4Shot13.webm'
+import Level4Shot13Ogv from '../videos/level4/Level4Shot13.ogv'
 import Level4Shot14Mp4 from '../videos/level4/Level4Shot14.mp4'
-import Level4Shot14Ogv from '../videos/level4/Level4Shot14.ogv'
 import Level4Shot14WebM from '../videos/level4/Level4Shot14.webm'
+import Level4Shot14Ogv from '../videos/level4/Level4Shot14.ogv'
 import Level4Shot15Mp4 from '../videos/level4/Level4Shot15.mp4'
-import Level4Shot15Ogv from '../videos/level4/Level4Shot15.ogv'
 import Level4Shot15WebM from '../videos/level4/Level4Shot15.webm'
+import Level4Shot15Ogv from '../videos/level4/Level4Shot15.ogv'
 import Level4Shot16Mp4 from '../videos/level4/Level4Shot16.mp4'
-import Level4Shot16Ogv from '../videos/level4/Level4Shot16.ogv'
 import Level4Shot16WebM from '../videos/level4/Level4Shot16.webm'
+import Level4Shot16Ogv from '../videos/level4/Level4Shot16.ogv'
 
 const data = [
   {
@@ -59,13 +59,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot1Ogv,
-        type: 'video/ogg'
+        src: Level4Shot1WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot1WebM,
-        type: 'video/webm'
+        src: Level4Shot1Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -80,13 +80,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -101,13 +101,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -122,13 +122,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot4Ogv,
-        type: 'video/ogg'
+        src: Level4Shot4WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot4WebM,
-        type: 'video/webm'
+        src: Level4Shot4Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -141,13 +141,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot4Ogv,
-        type: 'video/ogg'
+        src: Level4Shot4WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot4WebM,
-        type: 'video/webm'
+        src: Level4Shot4Ogv,
+        type: 'video/ogg'
       },
     ],
     options: [
@@ -174,13 +174,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot5Ogv,
-        type: 'video/ogg'
+        src: Level4Shot5WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot5WebM,
-        type: 'video/webm'
+        src: Level4Shot5Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -195,13 +195,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot6Ogv,
-        type: 'video/ogg'
+        src: Level4Shot6WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot6WebM,
-        type: 'video/webm'
+        src: Level4Shot6Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -216,13 +216,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot7Ogv,
-        type: 'video/ogg'
+        src: Level4Shot7WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot7WebM,
-        type: 'video/webm'
+        src: Level4Shot7Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -235,13 +235,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot7Ogv,
-        type: 'video/ogg'
+        src: Level4Shot7WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot7WebM,
-        type: 'video/webm'
+        src: Level4Shot7Ogv,
+        type: 'video/ogg'
       },
     ],
     options: [
@@ -268,13 +268,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot8Ogv,
-        type: 'video/ogg'
+        src: Level4Shot8WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot8WebM,
-        type: 'video/webm'
+        src: Level4Shot8Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -289,13 +289,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot9Ogv,
-        type: 'video/ogg'
+        src: Level4Shot9WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot9WebM,
-        type: 'video/webm'
+        src: Level4Shot9Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -310,13 +310,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot10Ogv,
-        type: 'video/ogg'
+        src: Level4Shot10WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot10WebM,
-        type: 'video/webm'
+        src: Level4Shot10Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -331,13 +331,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot11Ogv,
-        type: 'video/ogg'
+        src: Level4Shot11WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot11WebM,
-        type: 'video/webm'
+        src: Level4Shot11Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -352,13 +352,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot12Ogv,
-        type: 'video/ogg'
+        src: Level4Shot12WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot12WebM,
-        type: 'video/webm'
+        src: Level4Shot12Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -373,13 +373,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot13Ogv,
-        type: 'video/ogg'
+        src: Level4Shot13WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot13WebM,
-        type: 'video/webm'
+        src: Level4Shot13Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -394,13 +394,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot14Ogv,
-        type: 'video/ogg'
+        src: Level4Shot14WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot14WebM,
-        type: 'video/webm'
+        src: Level4Shot14Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -415,13 +415,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot15Ogv,
-        type: 'video/ogg'
+        src: Level4Shot15WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot15WebM,
-        type: 'video/webm'
+        src: Level4Shot15Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -434,13 +434,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot15Ogv,
-        type: 'video/ogg'
+        src: Level4Shot15WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot15WebM,
-        type: 'video/webm'
+        src: Level4Shot15Ogv,
+        type: 'video/ogg'
       },
     ],
     options: [
@@ -470,13 +470,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot5Ogv,
-        type: 'video/ogg'
+        src: Level4Shot5WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot5WebM,
-        type: 'video/webm'
+        src: Level4Shot5Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -489,13 +489,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot5Ogv,
-        type: 'video/ogg'
+        src: Level4Shot5WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot5WebM,
-        type: 'video/webm'
+        src: Level4Shot5Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Irei reforçar diariamente a avaliação da pele, a utilização da escala de Braden e a inspeção da presença de edemas com a equipe de enfermagem.',
@@ -524,13 +524,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -543,13 +543,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Também irei prescrever a utilização de solução hidratante seis vezes ao dia nas proeminências ósseas.',
@@ -578,13 +578,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot6Ogv,
-        type: 'video/ogg'
+        src: Level4Shot6WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot6WebM,
-        type: 'video/webm'
+        src: Level4Shot6Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -597,13 +597,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot6Ogv,
-        type: 'video/ogg'
+        src: Level4Shot6WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot6WebM,
-        type: 'video/webm'
+        src: Level4Shot6Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Vou prescrever uma rotina de fisioterapia motora para mobilizar o paciente no leito modificação seu decúbito, bem como o trabalho de fisioterapia respiratória para melhorar o padrão respeitório do paciente para conseguirmos retirá-lo do leito.',
@@ -632,13 +632,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -651,13 +651,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Será prescrito mudança de decúbito a cada duas horas. E orientações ao paciente e seus cuidados sobre a importância dessas mudanças.',
@@ -686,13 +686,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -705,13 +705,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Solicitarei uma avalição da nutricionista, a fim de promover o suporte nutricional adequado e avaliar o estado nutricional do paciente.',
@@ -740,13 +740,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -759,13 +759,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Irei prescrever a utilização de coberturas adequadas nas áreas de proeminências ósseas.',
@@ -794,13 +794,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -813,13 +813,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot3Ogv,
-        type: 'video/ogg'
+        src: Level4Shot3WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot3WebM,
-        type: 'video/webm'
+        src: Level4Shot3Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Irei prescrever a utilização de coxins para diminuir a pressão nas proeminências ósseas do paciente.',
@@ -848,13 +848,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -867,13 +867,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot2Ogv,
-        type: 'video/ogg'
+        src: Level4Shot2WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot2WebM,
-        type: 'video/webm'
+        src: Level4Shot2Ogv,
+        type: 'video/ogg'
       },
     ],
     dialogue: 'Também irei prescrever a manutenção da cabeceira do leito do paciente em 45 graus para melhorar o seu estado respiratório e reduzir as chances de cisalhamento do paciente no leito.',
@@ -902,13 +902,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot8Ogv,
-        type: 'video/ogg'
+        src: Level4Shot8WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot8WebM,
-        type: 'video/webm'
+        src: Level4Shot8Ogv,
+        type: 'video/ogg'
       },
     ]
   },
@@ -923,13 +923,13 @@ const data = [
       },
       {
         id: 2,
-        src: Level4Shot16Ogv,
-        type: 'video/ogg'
+        src: Level4Shot16WebM,
+        type: 'video/webm'
       },
       {
         id: 3,
-        src: Level4Shot16WebM,
-        type: 'video/webm'
+        src: Level4Shot16Ogv,
+        type: 'video/ogg'
       },
     ]
   }
